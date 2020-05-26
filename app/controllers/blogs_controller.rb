c5594dcf65d5485ec99dc11594751db8b04539db
codetalkertive <codetalkertive@gmail.com>
2020-05-26 13:36:45 +0100

    form-based feature added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,8 +7,44 @@ class BlogsController < ApplicationController
   end
 
   def show
+  	@blog = Blog.find(params[:id])
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	@blog = Blog.find(params[:id])
+
+  	if @blog.update(blog_params)
+  		redirect_to @blog
+  	else
+  	render :edit
+  	end
+
+  end
+
+  def create
+  	@blog = Blog.create(blog_params)
+
+  	if @blog.save
+  		redirect_to @blog
+  	else
+  		render :new
+  	end
+  end
+
+  def destroy
+  	@blog = Blog.find(params[:id])
+
+  	@blog.destroy
+  	redirect_to :action => :index
+  end
+
+  private
+
+  def blog_params
+  	params.require(:blog).permit(:title, :content)
   end
 end
